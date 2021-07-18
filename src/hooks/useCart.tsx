6ac8a95c966fe460aeb5dfffeffe470e5e165980
gabriel-github <gabrielferrsantos201@gmail.com>
2021-07-17 22:11:46 -0300

    finishing addToCart function

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { createContext, ReactNode, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,20 +23,46 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@Rocketseat:cart");
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart];
+      const product = updatedCart.find((product) => product.id === productId);
+
+      const stock = await api.get(`stock/${productId}`);
+      const currentAmount = stock.data.amount;
+      const amount = product ? product.amount : 0;
+      const amountUpdated = amount + 1;
+
+      if (amount > currentAmount) {
+        toast.error("Não possui essa quantidade no estoque");
+        return;
+      }
+
+      if (product) {
+        product.amount = amountUpdated;
+      } else {
+        const response = await api.get(`products/${productId}`);
+        const newProduct = {
+          ...response.data,
+          amount: 1,
+        };
+
+        updatedCart.push(newProduct);
+      }
+
+      setCart([...updatedCart]);
+      localStorage.setItem("@Rocketseat:cart", JSON.stringify(updatedCart));
     } catch {
-      // TODO
+      console.error("error");
     }
   };
 
